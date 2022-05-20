1411f69a3ac8f134e76111f75695f4722a85ac00
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2022-05-20 21:33:02 +0200

    remove misleading info

--- generate.sh
+++ generate.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-echo "currently logging network, press CTRL+C"
-echo "your output will be $1"
 sudo tshark -i any -Y 'http or tls.handshake.extensions_server_name or dns' -Tfields -e ip.src -e ip.dst -e tls.handshake.extensions_server_name -e dns.resp.name -e http.host -e dns.a -e dns.aaaa > $1
