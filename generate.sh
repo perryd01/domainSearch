d8b15ae1a3a18dde9b045ef7e5c27bc0723c3922
Dániel Miklós <daniel.miklos0@gmail.com>
2022-05-20 23:17:50 +0200

    remove tls

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 # -i any - any capture interface
 # -Y - filter for http and dns
 # -e - show fields: ip, dns, host etc...
-sudo tshark -i any -Y 'http or tls.handshake.extensions_server_name or dns' -Tfields -e ip.src -e ip.dst -e tls.handshake.extensions_server_name -e dns.resp.name -e http.host -e dns.a -e dns.aaaa > $1
+sudo tshark -i any -Y 'http or tls.handshake.extensions_server_name or dns' -Tfields -e ip.src -e ip.dst -e dns.resp.name -e http.host -e dns.a -e dns.aaaa > $1
